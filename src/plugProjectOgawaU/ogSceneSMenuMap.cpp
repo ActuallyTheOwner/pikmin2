98b6767ba449688eba02152182fd8e68d83a97c5
ActuallyTheOwner <wellswolelouie@yahoo.com>
2025-01-31 19:21:41 -0600

    Fix change accidentally put in main

--- src/plugProjectOgawaU/ogSceneSMenuMap.cpp
+++ src/plugProjectOgawaU/ogSceneSMenuMap.cpp
@@ -1,3 +1,4 @@
+#include "Drought/Screen/AlteredMapMenu.h"
 #include "og/newScreen/SMenu.h"
 #include "LoadResource.h"
 #include "nans.h"
@@ -65,7 +66,7 @@ void SMenuMap::doUserCallBackFunc(Resource::MgrCommand* command)
  */
 void SMenuMap::doCreateObj(JKRArchive* archive)
 {
-	registObj(new ObjSMenuMap("SMenuMap screen"), archive);
+	registObj(new Drought::Screen::AlteredMapMenu("SMenuMap screen"), archive);
 	setColorBG(0, 0, 0, ObjSMenuMap::msVal._1B);
 }
 
